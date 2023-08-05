617a97847a1be05336a3ad72e1c59d30d33e081b
Xelema <xelemayazi@gmail.com>
2023-08-05 16:42:20 +0200

    added bounding box for optimization + mesh structure

--- main_cuda.cu
+++ main_cuda.cu
@@ -20,14 +20,13 @@
 #include "denoiser.hu"
 #include "triangle.hu"
 
-__host__ __device__ HitInfo closest_hit(ray r, sphere* spheres, int nbSpheres, triangle* triangles, int nbTriangles){
-
+__host__ __device__ HitInfo closest_hit(ray r, sphere* spheres, int nbSpheres, mesh_info* mesh_list, int nbMeshes){
     HitInfo closestHit;
     closestHit.didHit=false;
     closestHit.dst=INFINITY; // rien touché pour l'instant
 
     // vérifie les intersections avec les sphères
-    for(int i=0; i < nbSpheres ; i++){
+    for(int i = 0; i < nbSpheres ; i++){
         sphere s = spheres[i];
         HitInfo hitInfo = hit_sphere(s.center, s.radius, r);
 
@@ -38,19 +37,28 @@ __host__ __device__ HitInfo closest_hit(ray r, sphere* spheres, int nbSpheres, t
     }
 
     // vérifie les intersections avec les triangles
-    for(int i=0; i < nbTriangles ; i++){
-        triangle tri = triangles[i];
-        HitInfo hitInfo = hit_triangle(tri, r);
+    for (int i = 0; i < nbMeshes; i++){
 
-        if (hitInfo.didHit && hitInfo.dst < closestHit.dst){
-            closestHit = hitInfo;
-            closestHit.mat = tri.mat;
-        }
+        // bounding box (optimisation)
+        if(!hit_BBox(r, mesh_list[i].bb)){
+                continue;
+            }
+
+        for (int j = 0; j < mesh_list[i].nbTriangles ; j++){
+            triangle tri = extended_tri_to_tri(mesh_list[i].triangle_list[j]);
+            tri.mat = mesh_list[i].mat;
+            HitInfo hitInfo = hit_triangle(tri, r);
+
+            if (hitInfo.didHit && hitInfo.dst < closestHit.dst){
+                closestHit = hitInfo;
+                closestHit.mat = tri.mat;
+            }
+        } 
     }
     return closestHit;
 }
 
-__device__ color ambient_occlusion(vec3 point, vec3 normal, sphere* spheres, int nbSpheres, triangle* triangles, int nbTriangles, curandState* globalState, int ind, double AO_intensity) {
+__device__ color ambient_occlusion(vec3 point, vec3 normal, sphere* spheres, int nbSpheres, mesh_info* mesh_list, int nbMeshes, curandState* globalState, int ind, double AO_intensity) {
     const int nbSamples = 1; // pour l'instant 1 suffit, à voir pour d'autres scènes
 
     color occlusion = BLACK;
@@ -60,7 +68,7 @@ __device__ color ambient_occlusion(vec3 point, vec3 normal, sphere* spheres, int
         vec3 hemisphereDir = add(normal, randomDir);
         ray occlusionRay = {point, vec3_normalize(hemisphereDir)};
 
-        HitInfo occlusionHit = closest_hit(occlusionRay, spheres, nbSpheres, triangles, nbTriangles);
+        HitInfo occlusionHit = closest_hit(occlusionRay, spheres, nbSpheres, mesh_list, nbMeshes);
 
         if (occlusionHit.didHit) {
             double distance = vec3_length(sub(occlusionHit.hitPoint, point));
@@ -75,10 +83,10 @@ __device__ color ambient_occlusion(vec3 point, vec3 normal, sphere* spheres, int
 }
 
 
-__device__ col_alb_norm tracer(ray r, int nbRebondMax, curandState* globalState, int ind, sphere* spheres, int nbSpheres, triangle* triangles, int nbTriangles, double AO_intensity, bool useAO){
+__device__ col_alb_norm tracer(ray r, int nbRebondMax, curandState* globalState, int ind, sphere* spheres, int nbSpheres, mesh_info* mesh_list, int nbMeshes, double AO_intensity, bool useAO){
 
     // cas des lumières
-    HitInfo hitInfo = closest_hit(r, spheres, nbSpheres, triangles, nbTriangles); 
+    HitInfo hitInfo = closest_hit(r, spheres, nbSpheres, mesh_list, nbMeshes); 
     if (hitInfo.didHit){
         if (hitInfo.mat.emissionStrength > 0){
             color HSL = rgb_to_hsl(hitInfo.mat.emissionColor);
@@ -96,7 +104,7 @@ __device__ col_alb_norm tracer(ray r, int nbRebondMax, curandState* globalState,
 
     for (int i = 0; i<nbRebondMax; i++){
 
-        HitInfo hitInfo = closest_hit(r, spheres, nbSpheres, triangles, nbTriangles);
+        HitInfo hitInfo = closest_hit(r, spheres, nbSpheres, mesh_list, nbMeshes);
 
         if (hitInfo.didHit){
             material mat = hitInfo.mat;
@@ -113,7 +121,7 @@ __device__ col_alb_norm tracer(ray r, int nbRebondMax, curandState* globalState,
                 incomingLight = add(incomingLight,multiply(emittedLight, rayColor));
                 rayColor = multiply(mat.diffuseColor, rayColor);
 
-                color occlusion = ambient_occlusion(hitInfo.hitPoint, hitInfo.normal, spheres, nbSpheres, triangles, nbTriangles, globalState, ind, AO_intensity);
+                color occlusion = ambient_occlusion(hitInfo.hitPoint, hitInfo.normal, spheres, nbSpheres, mesh_list, nbMeshes, globalState, ind, AO_intensity);
                 rayColor = multiply(rayColor, occlusion); // applique l'occlusion ambiante à la couleur du rayon
             }
 
@@ -132,8 +140,7 @@ __device__ col_alb_norm tracer(ray r, int nbRebondMax, curandState* globalState,
     return {incomingLight, hitInfo.mat.diffuseColor, hitInfo.normal};
 }
 
-
-__global__ void render_canva(color* canva, int largeur_image, int hauteur_image, int nbRayonParPixel, int nbRebondMax, camera cam, curandState* states, sphere* spheres, int nbSpheres, triangle* triangles, int nbTriangles, double AO_intensity, bool useAO, double focus_distance, double ouverture_x, double ouverture_y, color* normal_tab, color* albedo_tab) {
+__global__ void render_canva(color* canva, int largeur_image, int hauteur_image, int nbRayonParPixel, int nbRebondMax, camera cam, curandState* states, sphere* spheres, int nbSpheres, mesh_info* mesh_list, int nbMeshes, double AO_intensity, bool useAO, double focus_distance, double ouverture_x, double ouverture_y, color* normal_tab, color* albedo_tab) {
     
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
@@ -143,8 +150,7 @@ __global__ void render_canva(color* canva, int largeur_image, int hauteur_image,
         int pixel_index = j*largeur_image+i;
         col_alb_norm totalLight = {{BLACK, BLACK, BLACK}};
 
-        for (int k=0; k<nbRayonParPixel; ++k){
- 
+        for (int k = 0; k<nbRayonParPixel; ++k){
             double u = ((double)i + 0.5 + randomDouble(states, ind, -0.5, 0.5))/(largeur_image-1);
             double v = ((double)j + 0.5 + randomDouble(states, ind, -0.5, 0.5))/(hauteur_image-1);
             
@@ -152,11 +158,12 @@ __global__ void render_canva(color* canva, int largeur_image, int hauteur_image,
             double dy_ouverture = randomDouble(states, ind, -0.5, 0.5) * ouverture_y;
 
             ray r = get_ray(u, v, cam, focus_distance, dx_ouverture, dy_ouverture);
-            totalLight = add_col_alb_norm(totalLight, tracer(r, nbRebondMax, states, ind, spheres, nbSpheres, triangles, nbTriangles, AO_intensity, useAO)); 
+            totalLight = add_col_alb_norm(totalLight, tracer(r, nbRebondMax, states, ind, spheres, nbSpheres, mesh_list, nbMeshes, AO_intensity, useAO));
         }
 
-        // for denoiser
         canva[pixel_index] = write_color_canva(totalLight.e[0], nbRayonParPixel);
+
+        // for denoiser
         albedo_tab[pixel_index] = divide_scalar(totalLight.e[1], nbRayonParPixel);
         normal_tab[pixel_index] = divide_scalar(totalLight.e[2], nbRayonParPixel);
 
@@ -181,20 +188,20 @@ int main(){
     point3 target = {{0.3, -0.5, -3}}; // cible de la camera
     vec3 up = {{0, 1, 0}}; // permet de modifier la rotation selon l'axe z ({{0, 1, 0}} pour horizontal)
 
-    double focus_distance = 3; // distance de mise au point (depth of field)
+    double focus_distance = 1; // distance de mise au point (depth of field)
     double ouverture_x = 0.;
     double ouverture_y = 0.;
 
     //qualité et performance
-    int nbRayonParPixel = 10;
+    int nbRayonParPixel = 100;
     int nbRebondMax = 5;
     
-    int nbThreadsX = 8; // peut dépendre des GPU
-    int nbThreadsY = 8; 
+    int nbThreadsX = 16; // peut dépendre des GPU
+    int nbThreadsY = 16; 
 
-    bool useDenoiser = false;
+    bool useDenoiser = true;
 
-    bool useAO = false; // occlusion ambiante, rendu environ 2x plus lent
+    bool useAO = true; // occlusion ambiante, rendu environ 2x plus lent
     double AO_intensity = 3; // supérieur à 1 pour augmenter l'intensité
 
     //position des sphères dans la scène
@@ -224,80 +231,21 @@ int main(){
     time_t maintenant = time(NULL); // Obtenir l'heure actuelle
     struct tm *temps = localtime(&maintenant); // Convertir en structure tm
 
-    sprintf(nomFichier, "MESH_%dRAYS_%dRB_%02d-%02d_%02dh%02d.ppm", nbRayonParPixel, nbRebondMax-1, temps->tm_mday, temps->tm_mon + 1, temps->tm_hour, temps->tm_min);
-
-    ////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////
-
-    // assimp part
-    ////////////////////////////////////////////////////////
-
-    const struct aiScene* scene = aiImportFile("1tree_tri.obj", aiProcess_Triangulate);
-
-    if (scene == NULL) {
-        printf("Error parsing: %s\n", aiGetErrorString());
-        return false;
-    }
-
-    int total_triangles = 0;
-    for (int i = 0; i < scene->mNumMeshes; i++) {
-        const struct aiMesh* mesh = scene->mMeshes[i];
-        total_triangles += mesh->mNumFaces;
-    }
-    printf("num triangles dans mesh : %d\n", total_triangles);
-
-    triangle* h_triangle_list = (triangle*)malloc(total_triangles * sizeof(triangle));
-
-    int triangle_index = 0;
-
-    vec3 displacement = {{0.3, -1.1, -2.1}};
-
-    for (int i = 0; i < scene->mNumMeshes; i++) {
-        const struct aiMesh* mesh = scene->mMeshes[i];
-        const struct aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-
-        // Get the diffuse color of the material
-        aiColor4D aiColor;
-        if (AI_SUCCESS == aiGetMaterialColor(material, AI_MATKEY_COLOR_DIFFUSE, &aiColor)) {
-            printf("Diffuse color: %f %f %f\n", aiColor.r, aiColor.g, aiColor.b);
-        }
-
-        color my_col = {{aiColor.r, aiColor.g, aiColor.b}};
-
-
-        // Get the shininess of the material
-        float shininess;
-        if (AI_SUCCESS == aiGetMaterialFloat(material, AI_MATKEY_SHININESS, &shininess)) {
-            printf("Shininess: %f\n", shininess);
-        }
-
-        for (int j = 0; j < mesh->mNumFaces; j++) {
-            const struct aiFace face = mesh->mFaces[j];
-
-            triangle* tri = &h_triangle_list[triangle_index];
-
-            tri->A = add(aiVector3D_to_vec3(mesh->mVertices[face.mIndices[0]]), displacement);
-            printf("%f %f %f   ", tri->A.e[0], tri->A.e[1], tri->A.e[2]);
-            tri->B = add(aiVector3D_to_vec3(mesh->mVertices[face.mIndices[1]]), displacement);
-            printf("%f %f %f   ", tri->B.e[0], tri->B.e[1], tri->B.e[2]);
-            tri->C = add(aiVector3D_to_vec3(mesh->mVertices[face.mIndices[2]]), displacement);
-            printf("%f %f %f\n", tri->C.e[0], tri->C.e[1], tri->C.e[2]);
-            tri->mat = {my_col, BLACK, 0.0, (double)shininess};
-
-            printf("index : %d\n", triangle_index);
-
-            triangle_index++;
-        }
-    }
-
-    printf("triangle 1, A : %f %f %f\n", h_triangle_list[1].A.e[0], h_triangle_list[1].A.e[1], h_triangle_list[1].A.e[2]);
-
-    ////////////////////////////////////////////////////////
+    sprintf(nomFichier, "BB_MESH_%dRAYS_%dRB_%02d-%02d_%02dh%02d.ppm", nbRayonParPixel, nbRebondMax-1, temps->tm_mday, temps->tm_mon + 1, temps->tm_hour, temps->tm_min);
 
     int nbSpheres = sizeof(h_sphere_list) / sizeof(h_sphere_list[0]);
-    int nbTriangles = total_triangles;
 
-    printf("num triangles dans liste : %d\n", nbTriangles);
+    int nbMeshes;
+    mesh_info* h_mesh_list = load_geometry_data("model3D/1tree_little.obj", &nbMeshes);
+
+    int nbTrianglesTotal = 0;
+    for (int i=0; i<nbMeshes; i++){
+        nbTrianglesTotal += h_mesh_list[i].nbTriangles;
+    }
+
+    printf("\nNombre de triangles : %d\n", nbTrianglesTotal);
+    printf("Nombre de meshes : %d\n", nbMeshes);
+
     FILE *fichier = fopen(nomFichier, "w");
 
     // temps d'execution
@@ -347,21 +295,38 @@ int main(){
     color* normal_tab_device;
     cudaMalloc((void**)&normal_tab_device, (largeur_image * hauteur_image)*sizeof(color));
 
-    // alloue la mémoire pour d_sphere_list sur le device puis copie h_sphere_list (host) vers le device, optimisation
+    // alloue la mémoire pour d_sphere_list sur le device puis copie h_sphere_list (host) vers le device
     sphere* d_sphere_list;
     cudaMalloc((void**)&d_sphere_list, nbSpheres*sizeof(sphere));
     cudaMemcpy(d_sphere_list, h_sphere_list, nbSpheres*sizeof(sphere), cudaMemcpyHostToDevice);
+
+    // alloue la memoire pour la liste de mesh_info sur le device
+    mesh_info* d_mesh_list;
+    cudaMalloc((void**)&d_mesh_list, nbMeshes * sizeof(mesh_info));
+    cudaMemcpy(d_mesh_list, h_mesh_list, nbMeshes * sizeof(mesh_info), cudaMemcpyHostToDevice);
+
+    // afin de pouvoir free correctement les d_triangle_list
+    extended_triangle** d_triangle_list_array = (extended_triangle**)malloc(nbMeshes * sizeof(extended_triangle*));
     
-    // alloue la mémoire pour d_triangle_list sur le device puis copie h_triangle_list (host) vers le device, optimisation
-    triangle* d_triangle_list;
-    cudaMalloc((void**)&d_triangle_list, nbTriangles*sizeof(triangle));
-    cudaMemcpy(d_triangle_list, h_triangle_list, nbTriangles*sizeof(triangle), cudaMemcpyHostToDevice);
+    // copie correctement la liste de triangles de la struct mesh_info de l'host vers le device (grosse galere)
+    for (int i = 0; i < nbMeshes; i++) {
+        extended_triangle* d_triangle_list;
+        cudaMalloc((void**)&d_triangle_list, h_mesh_list[i].nbTriangles * sizeof(extended_triangle));
+        cudaMemcpy(d_triangle_list, h_mesh_list[i].triangle_list, h_mesh_list[i].nbTriangles * sizeof(extended_triangle), cudaMemcpyHostToDevice);
+        d_triangle_list_array[i] = d_triangle_list;
+
+        cudaMemcpy(&(d_mesh_list[i].triangle_list), &d_triangle_list_array[i], sizeof(extended_triangle*), cudaMemcpyHostToDevice);
+    }
+
+    for (int i = 0; i < nbMeshes; i++) {
+        cudaFree(d_triangle_list_array[i]);
+    }
 
     // initialise les "states" pour la fonction de random
     init_curand_state<<<blocks, threads>>>(states, largeur_image, hauteur_image);
 
     // lance le rendu de canva
-    render_canva<<<blocks, threads>>>(canva_device, largeur_image, hauteur_image, nbRayonParPixel, nbRebondMax, cam, states, d_sphere_list, nbSpheres, d_triangle_list, nbTriangles, AO_intensity, useAO, focus_distance, ouverture_x, ouverture_y, normal_tab_device, albedo_tab_device);
+    render_canva<<<blocks, threads>>>(canva_device, largeur_image, hauteur_image, nbRayonParPixel, nbRebondMax, cam, states, d_sphere_list, nbSpheres, d_mesh_list, nbMeshes, AO_intensity, useAO, focus_distance, ouverture_x, ouverture_y, normal_tab_device, albedo_tab_device);
 
     // copie canva du device (gpu) vers l'host (cpu), puis free la mémoire de canva sur device
     cudaMemcpy(canva, canva_device, (largeur_image * hauteur_image)*sizeof(color), cudaMemcpyDeviceToHost);
@@ -369,7 +334,7 @@ int main(){
 
     cudaMemcpy(albedo_tab, albedo_tab_device, (largeur_image * hauteur_image)*sizeof(color), cudaMemcpyDeviceToHost);
     cudaFree(albedo_tab_device);
-
+    
     cudaMemcpy(normal_tab, normal_tab_device, (largeur_image * hauteur_image)*sizeof(color), cudaMemcpyDeviceToHost);
     cudaFree(normal_tab_device);
 
@@ -384,14 +349,23 @@ int main(){
             fprintf(fichier, "%d %d %d\n", (int)(canva[j*largeur_image+i].e[0]), (int)(canva[j*largeur_image+i].e[1]), (int)(canva[j*largeur_image+i].e[2]));
         }
     }
-    
+
+    // check les erreurs CUDA
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
+    }
+
     fclose(fichier);
+
     cudaFree(d_sphere_list);
-    cudaFree(d_triangle_list);
+    cudaFree(d_mesh_list);
     cudaFree(states);
 
-    aiReleaseImport(scene);
-    free(h_triangle_list);
+    free(h_mesh_list);
+    free(canva);
+    free(albedo_tab);
+    free(normal_tab);
 
     // enregistrer le moment d'arrivée
     cudaEventRecord(stop, 0);
@@ -405,12 +379,6 @@ int main(){
 
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
-
-    // check les erreurs CUDA
-    cudaError_t error = cudaGetLastError();
-    if (error != cudaSuccess) {
-        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
-    }
 
 	return 0;
 }
